dd5d23c276522bcac0aefc2fb89a90ef63b22eb1
norab0 <boudarbalanora@gmail.com>
2025-09-02 12:17:10 +0200

    add router outlet to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { TodoListComponent } from './features/todos/components/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, TodoListComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   template: `
     <app-header></app-header>
     <main class="container mx-auto p-4">
