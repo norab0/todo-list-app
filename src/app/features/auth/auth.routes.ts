ee63650d3c394a782ceaf96dd26edaa17f724422
norab0 <boudarbalanora@gmail.com>
2025-09-02 22:00:05 +0200

    update routes auth

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,8 +1,20 @@
+// src/app/features/auth/auth.routes.ts
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login.component';
-import { RegisterComponent } from './components/register.component';
 
 export const AUTH_ROUTES: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    // TODO: Ajouter RegisterComponent dans la Partie 2
+    redirectTo: '/todos',
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
 ];
