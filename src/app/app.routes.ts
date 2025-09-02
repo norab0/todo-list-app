3d7a5f8da0cf39a63164be29a8f360120007abbc
norab0 <boudarbalanora@gmail.com>
2025-09-02 21:12:48 +0200

    creation of login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/todos', pathMatch: 'full' },
+  {
+    path: 'todos',
+    loadChildren: () => import('./features/todos/todos.routes').then((m) => m.TODOS_ROUTES),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin.routes').then((m) => m.ADMIN_ROUTES),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   template: `
     <header class="bg-blue-600 text-white p-4">
       <div class="container mx-auto flex justify-between items-center">
