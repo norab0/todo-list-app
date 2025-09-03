046dea7c2655cd3134e62b00bc107286c4fac0c1
norab0 <boudarbalanora@gmail.com>
2025-09-03 15:02:35 +0200

    register component

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 // src/app/features/auth/auth.routes.ts
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login.component';
+import { RegisterComponent } from './components/register.component';
 
 export const AUTH_ROUTES: Routes = [
   {
@@ -9,8 +10,7 @@ export const AUTH_ROUTES: Routes = [
   },
   {
     path: 'register',
-    // TODO: Ajouter RegisterComponent dans la Partie 2
-    redirectTo: '/todos',
+    component: RegisterComponent,
   },
   {
     path: '',
